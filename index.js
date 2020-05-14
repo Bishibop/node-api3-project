1091823826ec64f9bad7acec530c0107e5b53cdd
Nicholas Mullen <nwmullen@gmail.com>
2020-05-13 21:51:59 -0400

    mvp

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const userRoutes = require('./users/userRouter');
+
 const server = express();
 
+server.use(express.json());
+server.use(logger);
 server.use('/api/users', userRoutes);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log('Request info: ', req.method, req.url, new Date().toISOString());
+  next();
+}
+
+module.exports = server;
 
 server.listen(5000, () => 
   console.log('Server running on http://localhost:5000')
 );
-
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const userRoutes = require('./users/userRouter');
 
 const server = express();
+
+//server.use(logger);
+server.use('/api/users', userRoutes);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log('Request info: ', req.method, req.url, new Date().toISOString());
+  next();
+}
 
 module.exports = server;
